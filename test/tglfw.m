1786279f82689e6a52da615746b98c32f783f858
Mark Cafaro <mark.cafaro@gmail.com>
2021-06-06 14:54:59 -0700

    Added first test

---
+++ test/tglfw.m
@@ -0,0 +1,33 @@
+classdef tglfw < matlab.unittest.TestCase
+    methods (TestClassSetup)
+        function init(testCase)
+            testCase.applyFixture(PathFixture(["/usr/local/lib","/usr/local/include/GLFW"]));
+            if ~libisloaded("libglfw")
+                loadlibrary("libglfw", "glfw3");
+            end
+            r = calllib("libglfw", "glfwInit");
+            testCase.fatalAssertTrue(logical(r));
+        end
+    end
+    
+    methods (TestClassTeardown)
+        function terminate(~)
+            calllib("libglfw", "glfwTerminate");
+            unloadlibrary("libglfw");
+        end
+    end
+    
+    methods (Test)
+        function getVersion(testCase)
+            [major,minor,rev] = calllib("libglfw", "glfwGetVersion", 0, 0, 0);
+            testCase.log(1,sprintf("Running against GLFW %i.%i.%i", major, minor, rev));
+            testCase.verifyNotEmpty(major);
+            testCase.verifyNotEmpty(minor);
+            testCase.verifyNotEmpty(rev);
+        end
+    end
+end
+
+function f = PathFixture(varargin)
+f = matlab.unittest.fixtures.PathFixture(varargin{:});
+end
