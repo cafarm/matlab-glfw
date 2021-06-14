e868c92de81e27e5c5f47a12fd7f6c330cab265e
Mark Cafaro <mark.cafaro@gmail.com>
2021-06-13 17:47:45 -0700

    Test to verify correct glfw func called

---
+++ tests/tglfwCalllib.m
@@ -0,0 +1,25 @@
+classdef tglfwCalllib < matlab.unittest.TestCase
+    properties (TestParameter)
+        FuncName = allFuncNames();
+    end
+    
+    methods (Test)
+        function calllib(testCase, FuncName)
+            import matlab.unittest.constraints.Matches;
+            contents = fileread(fullfile(srcFolder, FuncName + ".m"));
+            testCase.verifyThat(contents, Matches(sprintf('calllib("libglfw",[ ]*"%s"', FuncName)));
+        end
+    end
+end
+
+function n = allFuncNames()
+listing = dir(fullfile(srcFolder, "glfw*"));
+n = string({listing.name});
+n = replace(n, ".m", "");
+n = cellstr(n);
+end
+
+function f = srcFolder()
+rootFolder = fileparts(fileparts(mfilename("fullpath")));
+f = fullfile(rootFolder, "src");
+end
