7f1dd89b4af7d9cb54ce071c9e30bc8ed3824712
Mark Cafaro <mark.cafaro@gmail.com>
2021-07-03 23:12:33 -0400

    Remove callback bindings

--- src/glfwSetKeyCallback.m
+++
@@ -1,6 +0,0 @@
-function [lib.pointer, GLFWwindowPtr, voidPtr] = glfwSetKeyCallback(GLFWwindowPtr, voidPtr)
-arguments
-    
-end
-[lib.pointer, GLFWwindowPtr, voidPtr] = calllibglfw("glfwSetKeyCallback", GLFWwindowPtr, voidPtr);
-end
--- src/glfwSetWindowSizeCallback.m
+++
@@ -1,6 +0,0 @@
-function [lib.pointer, GLFWwindowPtr, voidPtr] = glfwSetWindowSizeCallback(GLFWwindowPtr, voidPtr)
-arguments
-    
-end
-[lib.pointer, GLFWwindowPtr, voidPtr] = calllibglfw("glfwSetWindowSizeCallback", GLFWwindowPtr, voidPtr);
-end
