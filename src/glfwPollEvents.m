e551e3087572f5e10cc3073d566f16bc08b1ccdc
Mark Cafaro <mark.cafaro@gmail.com>
2021-07-16 22:22:26 -0400

    Flush event queue in polling functions

--- src/glfwPollEvents.m
+++ src/glfwPollEvents.m
@@ -1,3 +1,5 @@
 function glfwPollEvents()
 calllibglfw("glfwPollEvents");
+feature("qewait", -1);
+pause(0.0002);
 end
--- src/glfwWaitEvents.m
+++ src/glfwWaitEvents.m
@@ -1,3 +1,5 @@
 function glfwWaitEvents()
 calllibglfw("glfwWaitEvents");
+feature("qewait", -1);
+pause(0.0002);
 end
