e80991bb4e4672ebeb13881f3146666d627cebfa
Mark Cafaro <mark.cafaro@gmail.com>
2021-10-30 12:13:46 -0400

    Update to 3.3.5

--- getdeps.sh
+++ getdeps.sh
@@ -24,7 +24,7 @@ esac
 # Compile GLFW from source
 # You may need to pre-install some deps before this will work
 # See: https://www.glfw.org/docs/latest/compile.html#compile_deps
-git clone --depth 1 --branch 3.3.4 https://github.com/glfw/glfw.git
+git clone --depth 1 --branch 3.3.5 https://github.com/glfw/glfw.git
 cd glfw
 git apply ../patches/*.patch
 cmake -DCMAKE_INSTALL_PREFIX=../deps/glfw/${mw_arch} -DBUILD_SHARED_LIBS=ON -DCMAKE_OSX_ARCHITECTURES='arm64;x86_64' .
